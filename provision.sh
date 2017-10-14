d583988490e5e344fa47cdd2a1ed912ccd850f3d
Garrett Marcotte <garrett.marcotte@gmail.com>
2017-10-14 00:03:27 -0400

    Add hgrc, remove fb/local-specific stuff from bashrc, add provision script

---
+++ provision.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp dir_colors.txt ~/.dir_colors
+cp git-completion.bash ~/.git-completion.bash
+cp git-template.txt ~/.git-template
+cp scm-prompt.bash ~/.scm-prompt
+
+cat bash_profile.txt >> ~/.bash_profile
+cat bashrc.txt >> ~/.bashrc
+cat gitconfig.txt >> ~/.gitconfig
+cat hgrc.txt >> ~/.hgrc
+cat screenrc.txt >> ~/.screenrc
+cat vimrc.txt >> ~/.vimrc
