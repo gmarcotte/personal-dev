c8845043fcf6ce52dad913efee25b8e149f34e3b
Garrett Marcotte <garrett.marcotte@gmail.com>
2022-05-08 16:29:28 -0600

    Force symlink creation

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/discord/.dir_colors ~/.dir_colors
-ln -s ~/dotfiles/discord/.git-completion.bash ~/.git-completion.bash
-ln -s ~/dotfiles/discord/.git-template ~/.git-template
-ln -s ~/dotfiles/discord/.scm-prompt ~/.scm-prompt
+ln -sf ~/dotfiles/discord/.dir_colors ~/.dir_colors
+ln -sf ~/dotfiles/discord/.git-completion.bash ~/.git-completion.bash
+ln -sf ~/dotfiles/discord/.git-template ~/.git-template
+ln -sf ~/dotfiles/discord/.scm-prompt ~/.scm-prompt
 
-ln -s ~/dotfiles/discord/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/discord/.bashrc ~/.bashrc
-ln -s ~/dotfiles/discord/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/discord/.hgrc ~/.hgrc
-ln -s ~/dotfiles/discord/.screenrc ~/.screenrc
-ln -s ~/dotfiles/discord/.vimrc ~/.vimrc
-ln -s ~/dotfiles/discord/.zshrc ~/.zshrc
-ln -s ~/dotfiles/discord/.zprofile ~/.zprofiles
+ln -sf ~/dotfiles/discord/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/discord/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/discord/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/discord/.hgrc ~/.hgrc
+ln -sf ~/dotfiles/discord/.screenrc ~/.screenrc
+ln -sf ~/dotfiles/discord/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/discord/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/discord/.zprofile ~/.zprofiles
